75d09f3b7b9b1b77eb75a3c79540cbaa49c53b3a
MaitrikKaushik <maitrik2006.kaushik.morni@gmail.com>
2026-03-16 10:23:43 +0530

    feat: fix server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
 neuiwfn;
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+const app = express();
+
+// connect database
+connectDB();
+
+// middleware
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
